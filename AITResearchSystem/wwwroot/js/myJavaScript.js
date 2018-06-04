d13afa8bbf7251885003499df94c7611a7f0521d
Cliverson Obrzut <cliver_82@hotmail.com>
2018-06-04 23:07:29 +1000

    fixed the way table row hidden should be initialized when Admin page loads.

--- AITResearchSystem/wwwroot/js/myJavaScript.js
+++ AITResearchSystem/wwwroot/js/myJavaScript.js
@@ -22,30 +22,20 @@ $(document).ready(function () {
     fixFooter();
 });
 
+$(document).bind(function () {
+    fixFooter();
+});
 
 // when click in a visible row it display the second
 // row of the pair with more information about the 
 // respondent answers in Admin page
 $("#thetable tbody tr:even").on('click',
     function () {
-        console.log("clicked in a row");
         var row2 = $(this).closest('tr').next('tr');
-        console.log(row2.index());
-        if ($(row2).attr("value") === "false") {
-            console.log("true");
-            $(row2).css("display", "");
-            $(row2).attr("value","true");
+        if ($(row2).attr("style") === "display:none") {
+            $(row2).attr("style","");
         } else {
-            console.log("false");
-            $(row2).css("display", "none");
-            $(row2).attr("value", "false");
+            $(row2).attr("style", "display:none");
         }
+        fixFooter();
     });
-
-// every respondent answers are a pair of two rows.
-// when the page loads it hides the second row of the pair
-$(document).ready(function () {
-    $("[name=\"tr-hidden\"]").css("display", "none");
-    $("[name=\"tr-hidden\"]").attr("value","false");
-    console.log("added initial css and value");
-});
